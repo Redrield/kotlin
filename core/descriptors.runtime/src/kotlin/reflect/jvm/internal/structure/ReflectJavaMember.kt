Redrield/kotlin

commit 0f0602230ace0bd22aa33d0b7d47506a758b8899

Fix parameter names of Java inner classes in reflection
 #KT-25541 Fixed

core/descriptors.runtime/src/kotlin/reflect/jvm/internal/structure/ReflectJavaMember.kt
@@ -46,9 +46,13 @@ abstract class ReflectJavaMember : ReflectJavaElement(), ReflectJavaAnnotationOw
     ): List<JavaValueParameter> {
         val result = ArrayList<JavaValueParameter>(parameterTypes.size)
         val names = Java8ParameterNamesLoader.loadParameterNames(member)
+
+        // Skip synthetic parameters such as outer class instance
+        val shift = names?.size?.minus(parameterTypes.size) ?: 0
+
         for (i in parameterTypes.indices) {
             val type = ReflectJavaType.create(parameterTypes[i])
-            val name = names?.run { get(i) }
+            val name = names?.getOrNull(i + shift) ?: error("No parameter with index $i+$shift (name=$name type=$type) in $this")
             val isParamVararg = isVararg && i == parameterTypes.lastIndex
             result.add(ReflectJavaValueParameter(type, parameterAnnotations[i], name, isParamVararg))
         }

compiler/testData/codegen/java8/box/reflection/parameters/realParameterNames.kt
@@ -9,6 +9,10 @@ public class J {
     public void foo(int methodParam) {}
 
     public static void bar(J staticMethodParam) {}
+
+    class Inner {
+        public Inner(double innerParam, Object innerParam2) {}
+    }
 }
 
 // FILE: K.kt
@@ -19,6 +23,7 @@ fun box(): String {
     assertEquals(listOf("constructorParam"), ::J.parameters.map { it.name })
     assertEquals(listOf(null, "methodParam"), J::foo.parameters.map { it.name })
     assertEquals(listOf("staticMethodParam"), J::bar.parameters.map { it.name })
+    assertEquals(listOf(null, "innerParam", "innerParam2"), J::Inner.parameters.map { it.name })
 
     return "OK"
 }

compiler/testData/codegen/java8/box/reflection/parameters/synthesizedParameterNames.kt
@@ -8,6 +8,10 @@ public class J {
     public void foo(int methodParam) {}
 
     public static void bar(J staticMethodParam) {}
+
+    class Inner {
+        public Inner(double innerParam, Object innerParam2) {}
+    }
 }
 
 // FILE: K.kt
@@ -18,6 +22,7 @@ fun box(): String {
     assertEquals(listOf("arg0"), ::J.parameters.map { it.name })
     assertEquals(listOf(null, "arg0"), J::foo.parameters.map { it.name })
     assertEquals(listOf("arg0"), J::bar.parameters.map { it.name })
+    assertEquals(listOf(null, "arg1", "arg2"), J::Inner.parameters.map { it.name })
 
     return "OK"
 }

commit fff63e64e0048dccce86f89cbe235d97fa78f6ff

Fix ISE in ReflectJavaMember.getValueParameters on Java 6
The exception was incorrectly introduced in 0f0602230ac. We should not
fail when `names` is null, only when there's no element at the
corresponding index. On Java 6, `names` is always null. On Java 8,
`names` is never null (it's an empty list if the class was compiled
without "-parameters")

core/descriptors.runtime/src/kotlin/reflect/jvm/internal/structure/ReflectJavaMember.kt
@@ -52,7 +52,9 @@ abstract class ReflectJavaMember : ReflectJavaElement(), ReflectJavaAnnotationOw
 
         for (i in parameterTypes.indices) {
             val type = ReflectJavaType.create(parameterTypes[i])
-            val name = names?.getOrNull(i + shift) ?: error("No parameter with index $i+$shift (name=$name type=$type) in $this")
+            val name = names?.run {
+                getOrNull(i + shift) ?: error("No parameter with index $i+$shift (name=$name type=$type) in $this@ReflectJavaMember")
+            }
             val isParamVararg = isVararg && i == parameterTypes.lastIndex
             result.add(ReflectJavaValueParameter(type, parameterAnnotations[i], name, isParamVararg))
         }